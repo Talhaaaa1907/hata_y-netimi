41e8ea2bde64efa6a91fca76fc546db9682ac329
example <email@example.com>
2021-08-23 19:28:05 +0300

    ilk commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Try_Catch_Finally_ve_Mantıksal_Hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz...");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayı: " +sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " +ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+                int b = int.Parse("test");
+                int c = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz...");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Girilen sayı değer aralığında değil.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
